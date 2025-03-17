656d76e1ba3df82dbdea60db5461179a83ca8204
MaksimLazarevSergeevich <st128707@student.spbu.ru>
2025-03-17 17:43:20 +0300

    docs(bow): add comments to clarify functionality and improve code readability

--- include/bow.h
+++ include/bow.h
@@ -1,10 +1,13 @@
 #ifndef BOW_H
 #define BOW_H
 #include "weapon.h"
+// The Bow class is inherited from the Weapon class
 class Bow: public Weapon
 {
 public:
+    // The constructor will use the constructor of the Weapon class
     Bow();
-    void buffComboMultiplicate();
+    // Realisation of the Bow weapon enhancement
+    void buffComboMultiplicate() override;
 };
 #endif
--- src/bow.cpp
+++ src/bow.cpp
@@ -1,9 +1,14 @@
 #include "include/bow.h"
 
-Bow::Bow(): Weapon("Bow", 4, 5) {} /*name, damage, energy*/
+// Bow class constructor, initializing the weapon with predefined attributes
+Bow::Bow(): Weapon("Bow", 4, 5) {} /* name, damage, energy cost */
 
+// Buff ability for the Bow, increasing its damage and energy cost
 void Bow::buffComboMultiplicate()
 {
+    // Increase weapon damage by 10
     setDamageWeapon(getDamageWeapon() + 10);
+    
+    // Increase energy cost by 4
     setEnergyWeaponCost(getEnergyWeaponCost() + 4);
 }
