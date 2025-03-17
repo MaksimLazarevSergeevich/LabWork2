1f375e4619f629c634275463d2b13c9e9b65c095
MaksimLazarevSergeevich <st128707@student.spbu.ru>
2025-03-17 17:35:35 +0300

    refactor(berserk): Improved console output and added comments

--- include/berserk.h
+++ include/berserk.h
@@ -1,10 +1,14 @@
 #ifndef BERSERK_H
 #define BERSERK_H
 #include "hero.h"
+// The Berserk class inherited from the Hero class
 class Berserk: public Hero
 {
 public:
+    // Realisation of a special ability Berserk
     void specialAbility(Hero*) override;
+    
+    // A constructor that will use the constructor of the Hero class
     Berserk();
 };
 #endif
--- src/berserk.cpp
+++ src/berserk.cpp
@@ -1,18 +1,32 @@
 #include "include/berserk.h"
 
-Berserk::Berserk(): Hero("Berserk", 150, 20, 15) {}
+// Berserk class constructor, initializing the hero with predefined attributes
+Berserk::Berserk(): Hero("Berserk", 120, 20, 10) {} /* name, health, energy, default attack damage */
 
+// Special ability for the Berserk hero
 void Berserk::specialAbility(Hero* enemyHero)
 {   
+    std::cout << "\n===== BERSERK'S ULTIMATE =====\n";
+    // Check if the Berserk has enough health to use the ability
     if (getHealth() > 17)
     {
-        std::cout << "Berserk used ultimate on " <<  enemyHero->getName() << '\n'; 
+        std::cout << "Berserk unleashes his rage upon " << enemyHero->getName() << "!\n";
+        
+        // Berserk sacrifices 17 health points to deal 25 damage to the enemy
         TakeDamage(17);
         enemyHero->TakeDamage(25);
+        
+        // The player has gone, so we bet true
         setIsPlayerGo(true);
+        
+        std::cout << "Berserk sacrificed 17 health to deal 25 damage!\n";
+        std::cout << "Current Health: " << getHealth() << "\n";
     }
     else
     {
-        std::cout << "Not enough health\n";
+        // Notify the player if there is not enough health
+        std::cout << "\nNot enough health\n";
+        std::cout << "You need more than 17 health to use ultimate\n";
     }
+    std::cout << "==============================\n\n";
 }
