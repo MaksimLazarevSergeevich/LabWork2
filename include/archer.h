13742761d5e7459cda8cc36a31cafe9d3a2fcd2c
MaksimLazarevSergeevich <st128707@student.spbu.ru>
2025-03-17 17:32:31 +0300

    refactor(archer): Improved console output and added comments

--- include/archer.h
+++ include/archer.h
@@ -1,10 +1,13 @@
 #ifndef ARCHER_H
 #define ARCHER_H
 #include "hero.h"
+// The Archer class inherited from the Hero class
 class Archer: public Hero
 {
 public:
+    // A constructor that will use the constructor of the Hero class
     Archer();
+    // Realisation of a special ability Archer
     void specialAbility(Hero*) override;
 };
 #endif
--- src/archer.cpp
+++ src/archer.cpp
@@ -1,18 +1,36 @@
 #include "include/archer.h"
 
-Archer::Archer(): Hero("Archer", 80, 45, 5) {}
+// Archer class constructor, initializing the hero with predefined attributes
+Archer::Archer(): Hero("Archer", 80, 45, 5) {} /* name, health, energy, default attack damage */
 
+// Special ability for the Archer hero
 void Archer::specialAbility(Hero* enemyHero)
 {
+    std::cout << "\n===== ARCHER'S ULTIMATE =====\n";
+    // Check if the Archer has enough energy to use the ability
     if (getEnergy() >= 10)
     {
-        std::cout << "Archer used ultimate on " << enemyHero->getName() << '\n';
+        std::cout << "Archer fires a deadly shot at " << enemyHero->getName() << "!\n";
+        
+        // Deduct energy cost for the ability
         spendEnergy(10);
-        enemyHero->TakeDamage(enemyHero->getHealth() / 3);
+        
+        // Deal damage equal to one-third of the enemy's current health
+        int damage = enemyHero->getHealth() / 3;
+        enemyHero->TakeDamage(damage);
+        
+        // The player has gone, so we bet true
         setIsPlayerGo(true);
+
+        std::cout << "Damage dealt: " << damage << "\n";
+        std::cout << "Remaining Energy: " << getEnergy() << "\n";
+        
     }
     else
     {
-        std::cout << "Not enough energy\n";
+        // Notify the player if there is not enough energy
+        std::cout << "\nNot enough energy\n";
+        std::cout << "You need 10 energy to use ultimate\n";
     }
+    std::cout << "==============================\n\n";
 }
