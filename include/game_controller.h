aa2375e473b087884f526d0f2ed74273598828dd
MaksimLazarevSergeevich <st128707@student.spbu.ru>
2025-03-17 20:50:16 +0300

    feat(game_controller) Implementing a method that controls the game

--- include/game_controller.h
+++ include/game_controller.h
@@ -1,12 +1,13 @@
 #ifndef GAME_CONTROLLER_H
 #define GAME_CONTROLLER_H
+// This class controls the state of the game, so we include the game menu and game fighter here
+#include "game_menu.h"
+#include "game_fighter.h"
+// The class that controls the game
 class GameController
 {
 public:
+    // Launching the game
     void startGame();
-    void setPointChanger(int);
-    int getPointChanger();
-private:
-    int _pointChanger = 0;
 };
 #endif
--- src/game_controller.cpp
+++ src/game_controller.cpp
@@ -1,16 +1,25 @@
 #include "include/game_controller.h"
 
-void GameController::setPointChanger(int x)
-{
-    _pointChanger = x;
-}
-
-int GameController::getPointChanger()
-{
-    return _pointChanger;
-}
-
+//A method of starting the game until the player exits
 void GameController::startGame()
 {
+    //Create the Menu class
+    GameMenu menu;
     
+    // If the point is -1, the player is out of the game, so end the loop
+    while(menu.getPointChanger() != -1) 
+    {
+        //Showing the choice
+        menu.showMainMenu();
+
+        // If the point is equal to 1, then the player has made a choice and started the fight
+        if (menu.getPointChanger() == 1)
+        {
+            // Create the GameFighter class and pass the selected values to the constructor
+            GameFighter fight(menu.getCurrentHero(), menu.getCurrentWeapon(), menu.getItems());
+
+            // Let the battle begin
+            fight.startFight();
+        }
+    }
 }
